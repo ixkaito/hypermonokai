7c716163ce9bbdc739c47c52552b9f5b670c4d1e
Kite <ixkaito@gmail.com>
2016-07-17 18:36:06 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+exports.decorateConfig = (config) => {
+  return Object.assign({}, config, {
+    cursorColor: '#f8f8f0',
+    foregroundColor: '#f8f8f2',
+    backgroundColor: '#272822',
+    colors: [
+      '#272822',
+      '#d2304b',
+      '#a6e22e',
+      '#e6db74',
+      '#ae81ff',
+      '#f92672',
+      '#66d9ef',
+      '#d0d0d0',
+      '#75715e',
+      '#d2304b',
+      '#a6e22e',
+      '#e6db74',
+      '#ae81ff',
+      '#f92672',
+      '#66d9ef',
+      '#f8f8f2'
+    ]
+  });
+}
