7e4ae2eb86785d843fef02dba6ae84a86d1e616b
Kite <ixkaito@gmail.com>
2016-07-18 05:25:45 +0900

    v1.0.1

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 exports.decorateConfig = (config) => {
   return Object.assign({}, config, {
     cursorColor: '#a6e22e',
-    foregroundColor: '#f8f8f2',
     colors: [
       '#272822',
       '#d2304b',
@@ -10,7 +9,7 @@ exports.decorateConfig = (config) => {
       '#ae81ff',
       '#f92672',
       '#66d9ef',
-      '#75715e',
+      '#f8f8f2',
       '#272822',
       '#d2304b',
       '#a6e22e',
@@ -18,7 +17,7 @@ exports.decorateConfig = (config) => {
       '#ae81ff',
       '#f92672',
       '#66d9ef',
-      '#75715e'
+      '#f8f8f2'
     ]
   });
 }
